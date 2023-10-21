2921c53d587866540bf6b0bb93dc9a2aa7544909
YEAHx4 <developerstarlight@gmail.com>
2023-10-21 14:41:19 +0900

    colorize

--- src/main/java/io/yeahx4/Main.java
+++ src/main/java/io/yeahx4/Main.java
@@ -1,14 +1,18 @@
 package io.yeahx4;
 
 import io.yeahx4.event.CachedEventListener;
+import io.yeahx4.stdio.AnsiColor;
 import io.yeahx4.stdio.StdoutWriter;
 
 public final class Main {
     public static void main(String[] args) {
         StdoutWriter.getInstance().print("Hello ");
-        StdoutWriter.getInstance().println("World!");
-        StdoutWriter.getInstance().println("Welcome to FxOS.");
+        StdoutWriter.getInstance().println(" World!");
+        StdoutWriter.getInstance().println("Welcome to " + AnsiColor.PURPLE_BOLD + "FxOS." + AnsiColor.RESET);
 
+        try {
+            Thread.sleep(100);
+        } catch (Exception ignored) {}
         shutdown();
     }
 
